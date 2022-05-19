8b33f3a155f102de714cb885f48b4ab24ea78ed7
Alejandra Perdomo <alejandraperdomo605@yahoo.com>
2022-05-19 14:15:40 -0600

    first commit

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
-import { View, Text } from "react-native";
+import { View, Text,StyleSheet } from "react-native";
+import HomeScreen from "./screens/HomeScreen";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
+    <View style={styles.container}>
+      <HomeScreen/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+})
+
